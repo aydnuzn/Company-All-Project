3e71d3cb9502e31496af5b48f23d95ea97c85eaf
Aydın UZUN <uzun.aydinn@gmail.com>
2021-10-09 13:08:21 +0300

    add forgotPassword

--- src/main/java/com/works/controllers/LoginController.java
+++ src/main/java/com/works/controllers/LoginController.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
 public class LoginController {
 
-    @GetMapping("")
+    @GetMapping("/login")
     public String login(){
         return "login";
+    }
+
+    @GetMapping("/forgotpassword")
+    public String forgotpassword(){
+        return "forgotpassword";
     }
 
 }
