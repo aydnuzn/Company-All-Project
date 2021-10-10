f29f76cbc42b2e74d0f986d7651ce1b7a8f83907
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-10 18:38:51 +0300

    Customer Entity Update

--- src/main/java/com/works/entities/Customer.java
+++ src/main/java/com/works/entities/Customer.java
@@ -1,4 +1,17 @@
 package com.works.entities;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }
