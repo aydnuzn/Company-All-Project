f017e498e82353cc8034182d3b53c93f64688c7b
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-10 18:38:24 +0300

    Like Entity

--- src/main/java/com/works/entities/Like.java
+++ src/main/java/com/works/entities/Like.java
@@ -1,4 +1,31 @@
 package com.works.entities;
 
-public class Like {
+import com.works.entities.Customer;
+import com.works.entities.Product;
+import com.works.entities.listener.BaseEntity;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Like extends BaseEntity<String> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @OneToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer score;
+
+
+
+
 }
