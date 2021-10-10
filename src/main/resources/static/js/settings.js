7f99fbbb951f1e784fb790a242fa6e05b8cc0fd9
Mert DUMANLI <77790459+mertdumanlicse@users.noreply.github.com>
2021-10-10 12:48:38 +0300

    updateMap

--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -1,19 +1,68 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-var map, infoWindow;
+let liveLocation;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 6,
+
+    // Map options
+    var options = {
+        zoom: 8,
+        center: {lat: 41.015137, lng: 28.979530}
+    };
+
+    // New map
+    var map = new google.maps.Map(document.getElementById('map'), options);
+
+    // Listen for click on map
+    google.maps.event.addListener(map, 'click', function (event) {
+        var newContent = prompt("Note");
+        var newIconImage;
+        if (confirm("Görüntü bayrak olsun mu?")) {
+            newIconImage = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+        }
+
+        // Add marker
+        var newProps = {
+            coords: event.latLng,
+            iconImage: newIconImage,
+            content: '<h1>' + newContent + '</h1>'
+        };
+        addMarker(newProps);
     });
-    infoWindow = new google.maps.InfoWindow();
 
+    // Loop through markers
+    for (var i = 0; i < markers.length; i++) {
+        // Add marker
+        addMarker(markers[i]);
+    }
+
+    // Add Marker Function
+    function addMarker(props) {
+        var marker = new google.maps.Marker({
+            position: props.coords,
+            map: map,
+        });
+
+        // Check for customicon
+        if (props.iconImage) {
+            // Set icon image
+            marker.setIcon(props.iconImage);
+        }
+
+        // Check content
+        if (props.content) {
+            var infoWindow = new google.maps.InfoWindow({
+                content: props.content
+            });
+
+            marker.addListener('click', function () {
+                infoWindow.open(map, marker);
+            });
+        }
+    }
+
+    //Find Live Location
+    var infoWindow = new google.maps.InfoWindow();
     const locationButton = document.createElement("button");
-
-    locationButton.textContent = "Pan to Current Location";
+    locationButton.textContent = "Canlı Konumumu Bul";
     locationButton.classList.add("custom-map-control-button");
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
     locationButton.addEventListener("click", () => {
@@ -25,13 +74,11 @@ function initMap() {
                         lat: position.coords.latitude,
                         lng: position.coords.longitude,
                     };
-
                     infoWindow.setPosition(pos);
-                    infoWindow.setContent("Location found.");
+                    infoWindow.setContent("Konumunuz bulundu.");
                     infoWindow.open(map);
                     map.setCenter(pos);
-                    console.log("Kuzey Paralelleri : " + pos.lat);
-                    console.log("Doğu Meridyenleri : " + pos.lng);
+                    liveLocation = pos;
                 },
                 () => {
                     handleLocationError(true, infoWindow, map.getCenter());
@@ -53,3 +100,20 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     );
     infoWindow.open(map);
 }
+
+//Ajax ile veritabanından gelecek.
+// Array of markers
+var markers = [
+    {
+        coords: {lat: 42.4668, lng: -70.9495},
+        iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+        content: '<h1>Lynn MA</h1>'
+    },
+    {
+        coords: {lat: 42.8584, lng: -70.9300},
+        content: '<h1>Amesbury MA</h1>'
+    },
+    {
+        coords: {lat: 42.7762, lng: -71.0773}
+    }
+];
