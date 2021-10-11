2531f85980a06ca548d1a3244199134f4736916b
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-11 19:27:04 +0300

    Address Entity

--- src/main/java/com/works/entities/Address.java
+++ src/main/java/com/works/entities/Address.java
@@ -1,4 +1,31 @@
 package com.works.entities;
 
-public class Address {
+import com.works.entities.constant.address.City;
+import com.works.entities.constant.address.District;
+import com.works.entities.listener.BaseEntity;
+import com.works.entities.listener.BaseEntityNotCompany;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Address extends BaseEntityNotCompany<String> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String address_detail;
+
+    @OneToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(name = "city_id")
+    City city;
+
+    @OneToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(name = "district_id")
+    District district;
+
+    @ManyToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
 }
