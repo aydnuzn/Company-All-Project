d52b5a777b8eb674b57081cb2d916ff12f0224db
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-11 19:50:37 +0300

    Edit Company Extends Class

--- src/main/java/com/works/entities/Company.java
+++ src/main/java/com/works/entities/Company.java
@@ -1,19 +1,30 @@
 package com.works.entities;
 
-import com.works.entities.listener.BaseEntityCompany;
+import com.works.entities.constant.address.City;
+import com.works.entities.constant.address.District;
+import com.works.entities.listener.BaseEntityNotCompany;
 import lombok.Data;
 
 import javax.persistence.*;
 
 @Entity
 @Data
-public class Company extends BaseEntityCompany<String> {
+public class Company extends BaseEntityNotCompany<String> {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String company_name;
     private String company_address;
+
+    @OneToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(name = "city_id")
+    City city;
+
+    @OneToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(name = "district_id")
+    District district;
+
     private Integer company_sector;
     private String company_tel;
     private String company_logo;
