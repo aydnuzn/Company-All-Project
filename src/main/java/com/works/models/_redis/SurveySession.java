d08b25ff47b0e34f453963cd8390b334c5c02231
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-12 22:21:47 +0300

    Create SurveySession.java

---
+++ src/main/java/com/works/models/_redis/SurveySession.java
@@ -0,0 +1,16 @@
+package com.works.models._redis;
+
+import lombok.Data;
+import org.springframework.data.redis.core.RedisHash;
+
+import javax.persistence.Id;
+
+@Data
+@RedisHash("sessionsurvey")
+public class SurveySession {
+    @Id
+    private String id;
+    private String survey_title;
+
+
+}
