d9878aae7cb62bec31fa3731c95a06adac2d1253
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-12 22:15:27 +0300

    Update Survey.java Entity

--- src/main/java/com/works/entities/survey/Survey.java
+++ src/main/java/com/works/entities/survey/Survey.java
@@ -4,6 +4,8 @@ import com.works.entities.listener.BaseEntity;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
@@ -13,6 +15,10 @@ public class Survey extends BaseEntity<String> {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+
+    @NotEmpty
+    @NotNull
+    @Column(unique = true)
     private String survey_title;
 
     @OneToMany(mappedBy = "survey", cascade = CascadeType.DETACH)
