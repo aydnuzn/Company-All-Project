3919be87c62db9409c1917d796efa75c1c5529b9
Mert DUMANLI <77790459+mertdumanlicse@users.noreply.github.com>
2021-10-12 19:06:35 +0300

    Update

--- src/main/java/com/works/controllers/adminpanel/CustomerController.java
+++ src/main/java/com/works/controllers/adminpanel/CustomerController.java
@@ -1,7 +1,10 @@
 package com.works.controllers.adminpanel;
 
-import com.works.entities.Customer;
-import com.works.repositories._jpa.CustomerRepository;
+import com.works.entities.security.Role;
+import com.works.entities.security.User;
+import com.works.properties.CustomerInterlayer;
+import com.works.repositories._jpa.RoleRepository;
+import com.works.repositories._jpa.UserRepository;
 import com.works.utils.Util;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
@@ -13,53 +16,62 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin/customer")
 public class CustomerController {
 
     final String rvalue = "adminpanel/customer/";
-    final CustomerRepository customerRepository;
+    final UserRepository userRepository;
+    final RoleRepository roleRepository;
 
-    public CustomerController(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
-
-    @GetMapping("/list")
-    public String customerList() {
-        return rvalue + "customerlist";
+    public CustomerController(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     @GetMapping("")
     public String customer(Model model) {
-        model.addAttribute("customer", new Customer());
+        model.addAttribute("customerInterlayer", new CustomerInterlayer());
         model.addAttribute("isError", 0);
         return "adminpanel/customer/customeradd";
     }
 
     @PostMapping("/add")
-    public String customerAdd(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult, Model model) {
+    public String customerAdd(@Valid @ModelAttribute("customerInterlayer") CustomerInterlayer customerInterlayer, BindingResult bindingResult, Model model) {
         if (!bindingResult.hasErrors()) {
-            if (Util.isEmail(customer.getCu_mail())) {
-                if (Util.isTel(customer.getCu_tel())) {
-                    try {
-                        customerRepository.save(customer);
-                    } catch (DataIntegrityViolationException e) {
-                        System.err.println(e);
-                        if (customerRepository.findByCu_mailEquals(customer.getCu_mail()).isPresent()) {
-                            model.addAttribute("isError", 2);
-                        } else {
-                            model.addAttribute("isError", 4);
-                        }
-                        return rvalue + "customeradd";
-                    }
+            User customer = new User();
+            customer.setName(customerInterlayer.getCu_name());
+            customer.setSurname(customerInterlayer.getCu_surname());
+            customer.setEmail(customerInterlayer.getCu_email());
+            customer.setPassword(customerInterlayer.getCu_password());//Security Eklendiğinde Şifrelenecek.
+            customer.setTel(customerInterlayer.getCu_tel());
+            customer.setCu_status(customerInterlayer.getCu_status());
+            customer.setEnabled(true);
+            customer.setTokenExpired(true);
+            customer.setAddresses(null);
+            Optional<Role> optRole = roleRepository.findById(3);
+            List<Role> roleList = new ArrayList<>();
+            if (optRole.isPresent()) {
+                roleList.add(optRole.get());
+                customer.setRoles(roleList);
+            } else {
+                //Veritabanında Müşteri Rolü Eksik.
+                customer.setRoles(null);
+            }
+            try {
+                userRepository.save(customer);
+            } catch (DataIntegrityViolationException e) {
+                if (userRepository.findByEmailEquals(customer.getEmail()).isPresent()) {
+                    model.addAttribute("isError", 1);
+                    return rvalue + "customeradd";
                 } else {
-                    model.addAttribute("isError", 3);
+                    model.addAttribute("isError", 2);
                     return rvalue + "customeradd";
                 }
-            } else {
-                model.addAttribute("isError", 1);
-                return rvalue + "customeradd";
             }
             return "redirect:/admin/customer";
         } else {
--- src/main/java/com/works/properties/RegisterInterlayer.java
+++ src/main/java/com/works/properties/RegisterInterlayer.java
@@ -1,6 +1,12 @@
 package com.works.properties;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class RegisterInterlayer {
@@ -8,10 +14,35 @@ public class RegisterInterlayer {
     private String company_address;
     private Integer company_sector;
     private String company_tel;
+
+    @NotNull
+    @NotEmpty
+    @Length(min = 2, max = 20)
     private String admin_name;
+
+    @NotNull
+    @NotEmpty
+    @Length(min = 2, max = 20)
     private String admin_surname;
+
+    @NotNull
+    @NotEmpty
+    @Email
     private String admin_email;
+
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "^\\d{11}$")
     private String admin_tel;
+
+    @NotNull
+    @NotEmpty
+    @Length(min = 4, max = 16)
     private String admin_password1;
+
+    @NotNull
+    @NotEmpty
+    @Length(min = 4, max = 16)
     private String admin_password2;
 }
+//Firma - Yönetici Ekleme
