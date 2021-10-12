040fab444e38f531d5e0da2db6b800e980dbcdfd
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-12 17:46:46 +0300

    Content Entity Update

--- src/main/java/com/works/entities/Content.java
+++ src/main/java/com/works/entities/Content.java
@@ -2,8 +2,11 @@ package com.works.entities;
 
 import com.works.entities.listener.BaseEntity;
 import lombok.Data;
+import lombok.NonNull;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
@@ -13,8 +16,15 @@ public class Content extends BaseEntity<String> {
     @Column(name = "id", nullable = false)
     private Integer id;
 
+    @NotNull
+    @NotEmpty
     private String content_title;
+    @NotNull
+    @NotEmpty
     private String content_brief_description;
+    @NotNull
+    @NotEmpty
     private String content_detailed_description;
+    @NotNull
     private Integer content_status;
 }
