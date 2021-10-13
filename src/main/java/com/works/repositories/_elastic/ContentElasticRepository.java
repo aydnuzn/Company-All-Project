1c0adcef9cd83a91ee4130f39b25a645dc2f760d
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-13 20:02:39 +0300

    Content Elasticsearch&Redis Repo Added

---
+++ src/main/java/com/works/repositories/_elastic/ContentElasticRepository.java
@@ -0,0 +1,17 @@
+package com.works.repositories._elastic;
+
+import com.works.models._elastic.ContentElasticsearch;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.elasticsearch.annotations.Query;
+
+import java.util.List;
+
+public interface ContentElasticRepository extends ElasticsearchRepository<ContentElasticsearch, String> {
+    @Query(value = "{\"bool\":{\"must\":{\"match\":{\"content_title\":\"?0\"}}}}")
+    Page<ContentElasticsearch> findByContent_title(String key, Pageable pageable);
+
+    @Query(value = "{\"bool\":{\"must\":{\"match\":{\"content_title\":\"?0\"}}}}")
+    List<ContentElasticsearch> findByContent_title(String key);
+}
---
+++ src/main/java/com/works/repositories/_redis/ContentSessionRepository.java
@@ -0,0 +1,17 @@
+package com.works.repositories._redis;
+
+import com.works.models._redis.ContentSession;
+import com.works.models._redis.SurveySession;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+@EnableRedisRepositories
+public interface ContentSessionRepository extends CrudRepository<ContentSession, String> {
+
+    @Query("select s from sessioncontent s order by c.id")
+    List<ContentSession> findByOrderByIdAsc(Pageable pageable);
+}
