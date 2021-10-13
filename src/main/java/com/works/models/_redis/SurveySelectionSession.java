235238806891dca944a1c4e36a7d4a6921830c07
Mert DUMANLI <77790459+mertdumanlicse@users.noreply.github.com>
2021-10-13 23:54:53 +0300

    Create SurveySelectionSession.java

---
+++ src/main/java/com/works/models/_redis/SurveySelectionSession.java
@@ -0,0 +1,18 @@
+package com.works.models._redis;
+
+import lombok.Data;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import javax.persistence.Id;
+
+@Data
+@RedisHash("sessionsurveyselection")
+public class SurveySelectionSession {
+    @Id
+    private String id;
+    private String survey_selection_title;
+    private Integer survey_selection_score;
+    @Indexed
+    private String surveyid;
+}
