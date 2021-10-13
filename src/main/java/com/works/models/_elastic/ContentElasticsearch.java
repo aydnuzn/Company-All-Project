b60f1ae7e4b7df7837ce9213cc406e1ecb215479
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-13 20:00:56 +0300

    Create ContentElasticsearch.java

---
+++ src/main/java/com/works/models/_elastic/ContentElasticsearch.java
@@ -0,0 +1,23 @@
+package com.works.models._elastic;
+
+import lombok.Data;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import javax.persistence.Id;
+
+@Document(indexName = "contentelasticsearch")
+@Data
+public class ContentElasticsearch {
+    @Id
+    private String id;
+    @Field(type = FieldType.Text)
+    private String content_title;
+    @Field(type = FieldType.Text)
+    private String content_brief_description;
+    @Field(type = FieldType.Text)
+    private String content_detailed_description;
+    @Field(type = FieldType.Text)
+    private String content_status;
+}
