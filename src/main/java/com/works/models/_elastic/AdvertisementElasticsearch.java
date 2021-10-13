e93c241c10c387dce5d4f384a3640711fe4d43e3
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-14 00:40:37 +0300

    Create AdvertisementElasticsearch.java

---
+++ src/main/java/com/works/models/_elastic/AdvertisementElasticsearch.java
@@ -0,0 +1,31 @@
+package com.works.models._elastic;
+
+import lombok.Data;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import javax.persistence.Id;
+
+
+@Data
+@Document(indexName = "advertisementelasticsearch")
+public class AdvertisementElasticsearch {
+    @Id
+    private String id;
+    @Field(type = FieldType.Text)
+    private String adv_title;
+    @Field(type = FieldType.Text)
+    private String adv_shown_number;
+    @Field(type = FieldType.Text)
+    private String adv_date_begin;
+    @Field(type = FieldType.Text)
+    private String adv_date_end;
+    @Field(type = FieldType.Text)
+    private String adv_image;
+    @Field(type = FieldType.Text)
+    private String adv_width;//genişlik
+    @Field(type = FieldType.Text)
+    private String adv_height;
+    @Field(type = FieldType.Text)
+    private String adv_link;
+}
