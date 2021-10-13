0e4df3ae0507c30bcd4ddae0794758db8a03ddcf
Aydın UZUN <uzun.aydinn@gmail.com>
2021-10-13 07:49:09 +0300

    add ProductAdd Controller

--- src/main/java/com/works/controllers/adminpanel/ProductController.java
+++ src/main/java/com/works/controllers/adminpanel/ProductController.java
@@ -1,37 +1,126 @@
 package com.works.controllers.adminpanel;
 
+import com.works.entities.Product;
+import com.works.entities.categories.ProductCategory;
+import com.works.entities.images.ProductImage;
+import com.works.properties.ProductCategoryInterlayer;
+import com.works.properties.ProductInterlayer;
+import com.works.repositories._jpa.ProductCategoryRepository;
+import com.works.repositories._jpa.ProductImageRepository;
+import com.works.repositories._jpa.ProductRepository;
+import com.works.utils.Util;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/admin/product")
 public class ProductController {
 
     final String rvalue = "adminpanel/product/";
-	final ProductCategoryRepository productCategoryRepository;
+
+    final ProductCategoryRepository productCategoryRepository;
     final ProductRepository productRepository;
-	
-	public ProductController(ProductCategoryRepository productCategoryRepository, ProductRepository productRepository) {
+    final ProductImageRepository productImageRepository;
+
+    public ProductController(ProductCategoryRepository productCategoryRepository, ProductRepository productRepository, ProductImageRepository productImageRepository) {
         this.productCategoryRepository = productCategoryRepository;
         this.productRepository = productRepository;
+        this.productImageRepository = productImageRepository;
     }
-	
-	@GetMapping("")
+
+    @GetMapping("")
     public String product(Model model) {
         model.addAttribute("productInterlayer", new ProductInterlayer());
         model.addAttribute("ls", productCategoryRepository.getAllMainCategory());
         return "adminpanel/product/productadd";
     }
 
-
     @GetMapping("/list")
     public String productList() {
         return rvalue + "productlist";
     }
 
-    @GetMapping("/add")
-    public String productAdd() {
+    @Transactional
+    @PostMapping("/add")
+    public String productAdd(@Valid @ModelAttribute("productInterlayer") ProductInterlayer productInterlayer, BindingResult bindingResult, Model model, @RequestPart(value = "pr_image_file", required = false) MultipartFile pr_image_file) {
+        Product product = new Product();
+        if(!bindingResult.hasErrors()){
+            List<ProductCategory> productCategoryList = new ArrayList<>();
+            for (int i = 0; i < productInterlayer.getPr_categories().size(); i++){
+                Optional<ProductCategory> optProductCategory = productCategoryRepository.findById(productInterlayer.getPr_categories().get(i));
+                if(optProductCategory.isPresent()){
+                    productCategoryList.add(optProductCategory.get());
+                }
+            }
+            try{
+                product.setPr_categories(productCategoryList);
+                product.setCompany(null);
+                product.setImages(null);
+
+                product.setPr_name(productInterlayer.getPr_name());
+                product.setPr_brief_description(productInterlayer.getPr_brief_description());
+                product.setPr_description(productInterlayer.getPr_description());
+                product.setPr_price(productInterlayer.getPr_price());
+                product.setPr_type(productInterlayer.getPr_type());
+                product.setPr_campaign(productInterlayer.getPr_campaign());
+                product.setPr_campaign_name(productInterlayer.getPr_campaign_name());
+                product.setPr_campaign_description(productInterlayer.getPr_campaign_description());
+                product.setPr_address(productInterlayer.getPr_address());
+                product.setPr_latitude(productInterlayer.getPr_latitude());
+                product.setPr_longitude(productInterlayer.getPr_longitude());
+                product = productRepository.save(product);
+
+                // Urunun resimlerinin tutulacagi Klasör olusturulacak
+                File theDir = new File(Util.UPLOAD_DIR + "products/" + product.getId());
+                if (!theDir.exists()) {
+                    theDir.mkdirs();
+                }
+
+                String fileName = StringUtils.cleanPath(pr_image_file.getOriginalFilename());
+                String ext = "";
+                try {//File kısmı validation'da kontrol edilmediği için resim yüklenmemesi durumu kontrolü
+                    int length = fileName.lastIndexOf(".");
+                    ext = fileName.substring(length, fileName.length());
+                    String uui = UUID.randomUUID().toString();
+                    fileName = uui + ext;
+
+                    Path path = Paths.get(Util.UPLOAD_DIR + "products/" + product.getId() + "/" + fileName);
+                    Files.copy(pr_image_file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+                } catch (Exception e) {
+                    //File yuklenmediyse, onceden ayarlanmis bir resim bu klasöre yüklenebilir.
+                    //...
+                }
+                ProductImage productImage = new ProductImage();
+                productImage.setProduct(product);
+                productImage.setImage_url(fileName);
+                productImageRepository.save(productImage);
+
+            }catch(Exception ex){
+                System.err.println("ProductAdd Error : " + ex);
+                return rvalue + "productadd";
+            }
+            return "redirect:/admin/product";
+        }else{
+            System.err.println(Util.errors(bindingResult));
+        }
+        model.addAttribute("ls", productCategoryRepository.getAllMainCategory());
         return rvalue + "productadd";
     }
 
