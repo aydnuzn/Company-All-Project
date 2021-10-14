697ee99225a655e0d7c6e1736f1c34482718808f
Aydın UZUN <uzun.aydinn@gmail.com>
2021-10-14 07:57:27 +0300

    Update AnnouncementSessionRepository

--- src/main/java/com/works/repositories/_redis/AnnouncementSessionRepository.java
+++ src/main/java/com/works/repositories/_redis/AnnouncementSessionRepository.java
@@ -1,9 +1,16 @@
 package com.works.repositories._redis;
 
 import com.works.models._redis.AnnouncementSession;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 @EnableRedisRepositories
 public interface AnnouncementSessionRepository extends CrudRepository<AnnouncementSession,String> {
+
+    @Query("select s from sessionannouncement s order by s.id")
+    List<AnnouncementSession> findByOrderByIdAsc(Pageable pageable);
 }
