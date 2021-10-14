cb155fc509eef0ca765fb6dd413566d15bd2a7f0
Aydın UZUN <uzun.aydinn@gmail.com>
2021-10-14 07:53:18 +0300

    create AnnouncementElastic

---
+++ src/main/java/com/works/models/_elastic/AnnouncementElastic.java
@@ -0,0 +1,25 @@
+package com.works.models._elastic;
+
+import lombok.Data;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import javax.persistence.Id;
+
+@Document(indexName = "announcementelastic")
+@Data
+public class AnnouncementElastic {
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String ann_title;
+
+    @Field(type=FieldType.Text)
+    private String ann_brief_description;
+
+    @Field(type=FieldType.Text)
+    private String ann_type;
+
+}
