e7a007cba877565567341ab7f4ec5782f176abb3
Aydın UZUN <uzun.aydinn@gmail.com>
2021-10-14 07:55:17 +0300

    create AnnouncementElasticRepository

---
+++ src/main/java/com/works/repositories/_elastic/AnnouncementElasticRepository.java
@@ -0,0 +1,18 @@
+package com.works.repositories._elastic;
+
+import com.works.models._elastic.AnnCategoryElastic;
+import com.works.models._elastic.AnnouncementElastic;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface AnnouncementElasticRepository extends ElasticsearchRepository<AnnouncementElastic, String> {
+    @Query(value = "{\"bool\":{\"must\":{\"match\":{\"ann_title\":\"?0\"}}}}")
+    Page<AnnouncementElastic> findByAnn_title(String key, Pageable pageable);
+
+    @Query(value = "{\"bool\":{\"must\":{\"match\":{\"ann_title\":\"?0\"}}}}")
+    List<AnnouncementElastic> findByAnn_title(String key);
+}
