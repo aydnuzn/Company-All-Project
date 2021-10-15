f8a064ad5a0b8b686f507d0fb194ee3b86709f64
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-15 21:22:09 +0300

    Update ContentController.java

--- src/main/java/com/works/controllers/adminpanel/ContentController.java
+++ src/main/java/com/works/controllers/adminpanel/ContentController.java
@@ -7,15 +7,14 @@ import com.works.repositories._elastic.ContentElasticRepository;
 import com.works.repositories._jpa.ContentRepository;
 import com.works.repositories._redis.ContentSessionRepository;
 import com.works.utils.Util;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin/content")
@@ -81,6 +80,74 @@ public class ContentController {
 
 
         return "redirect:/admin/content";
+    }
+
+    @PostMapping("/update/{stIndex}")
+    public String contentUpdate(@Valid @ModelAttribute("content") Content content, BindingResult bindingResult, Model model, @PathVariable String stIndex) {
+        Integer index = 0;
+        try {
+            index = Integer.parseInt(stIndex);
+        } catch (Exception e) {
+            return "error/404";
+        }
+        if (!bindingResult.hasErrors()) {
+            try {
+                content.setId(index);
+                contentRepository.saveAndFlush(content);
+
+                //REDIS
+                String contentId = String.valueOf(contentRepository.save(content).getId());
+                ContentSession contentSession = new ContentSession();
+                contentSession.setContent_title(content.getContent_title());
+                contentSession.setContent_brief_description(content.getContent_brief_description());
+                contentSession.setContent_detailed_description(content.getContent_detailed_description());
+                contentSession.setContent_status(String.valueOf(content.getContent_status()));
+                contentSession.setId(stIndex);
+                contentSessionRepository.deleteById(stIndex);
+                contentSessionRepository.save(contentSession);
+
+                //ELASTICSEARCH
+                ContentElasticsearch contentElasticsearch = new ContentElasticsearch();
+                contentElasticsearch.setContent_title(content.getContent_title());
+                contentElasticsearch.setContent_brief_description(content.getContent_brief_description());
+                contentElasticsearch.setContent_detailed_description(content.getContent_detailed_description());
+                contentElasticsearch.setContent_status(String.valueOf(content.getContent_status()));
+                contentElasticsearch.setId(stIndex);
+                contentElasticRepository.deleteById(stIndex);
+                contentElasticRepository.save(contentElasticsearch);
+            } catch (DataIntegrityViolationException ex) {
+                System.err.println("Error: " + ex);
+                model.addAttribute("isError", 1);
+                return rvalue + "contentupdate";
+            }
+        } else {
+            System.out.println(Util.errors(bindingResult));
+            model.addAttribute("isError", 0);
+            return rvalue + "contentupdate";
+        }
+        return "redirect:/admin/content/" + stIndex;
+    }
+
+
+    //update sayfasını açmak
+    @GetMapping("/{stIndex}")
+    public String contentUpdate(@PathVariable String stIndex, Model model) {
+        Integer index = 0;
+        try {
+            index = Integer.parseInt(stIndex);
+            Optional<Content> optionalContent = contentRepository.findById(index);
+            if (optionalContent.isPresent()) {
+                model.addAttribute("content", optionalContent.get());
+                model.addAttribute("index", index);
+                model.addAttribute("isError", 0);
+                return "adminpanel/content/contentupdate";
+            } else {
+                return "error/404";
+            }
+
+        } catch (Exception e) {
+            return "error/404";
+        }
     }
 }
 
