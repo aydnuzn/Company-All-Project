5fe59ca7d876aad6c273597ad7441cdbdd56ea2c
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-15 21:23:32 +0300

    Update ContentSession.java

--- src/main/java/com/works/models/_redis/ContentSession.java
+++ src/main/java/com/works/models/_redis/ContentSession.java
@@ -1,7 +1,9 @@
 package com.works.models._redis;
 
+import com.works.utils.Util;
 import lombok.Data;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 import javax.persistence.Id;
 
@@ -13,5 +15,7 @@ public class ContentSession {
     private String content_title;
     private String content_brief_description;
     private String content_detailed_description;
-    private String content_status; //bu String type olarak değişebilir
+    private String content_status;
+    @Indexed
+    private String companyname = Util.theCompany == null ? "":Util.theCompany.getCompany_name() ;
 }
