604a203230531cea0ee1eb6a0bbf1e1c3e8e0911
Aydın UZUN <uzun.aydinn@gmail.com>
2021-10-15 18:01:31 +0300

    Update AnnouncementCategory.java

--- src/main/java/com/works/entities/categories/AnnouncementCategory.java
+++ src/main/java/com/works/entities/categories/AnnouncementCategory.java
@@ -3,8 +3,11 @@ package com.works.entities.categories;
 import com.works.entities.Announcement;
 import com.works.entities.listener.BaseEntity;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
@@ -15,6 +18,9 @@ public class AnnouncementCategory extends BaseEntity<String> {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotNull
+    @NotEmpty
+    @Length(min = 3, max = 50, message = "En az 3, en fazla 50 karakter girilebilir.")
     @Column(unique = true)
     private String ann_category_title;
 
