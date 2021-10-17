9edf3c94729f3e5cb5f2d9d3b7ae8648019c3515
Mert DUMANLI <77790459+mertdumanlicse@users.noreply.github.com>
2021-10-17 15:49:22 +0300

    Update SurveySession.java

--- src/main/java/com/works/models/_redis/SurveySession.java
+++ src/main/java/com/works/models/_redis/SurveySession.java
@@ -1,7 +1,9 @@
 package com.works.models._redis;
 
+import com.works.utils.Util;
 import lombok.Data;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 import javax.persistence.Id;
 
@@ -11,4 +13,6 @@ public class SurveySession {
     @Id
     private String id;
     private String survey_title;
+    @Indexed
+    private String companyname = Util.theCompany.getCompany_name();
 }
