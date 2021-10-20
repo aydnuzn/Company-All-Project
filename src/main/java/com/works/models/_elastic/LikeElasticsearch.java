beed521d5cd129eb91be8ed16944ff5975266a9f
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-20 23:40:59 +0300

    Update LikeElasticsearch.java

--- src/main/java/com/works/models/_elastic/LikeElasticsearch.java
+++ src/main/java/com/works/models/_elastic/LikeElasticsearch.java
@@ -1,9 +1,10 @@
 package com.works.models._elastic;
 
+import lombok.Data;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 import javax.persistence.Id;
-
+@Data
 @Document(indexName = "likeelasticsearch")
 public class LikeElasticsearch {
     @Id
