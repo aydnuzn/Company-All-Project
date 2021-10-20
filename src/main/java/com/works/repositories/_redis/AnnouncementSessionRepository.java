997aac7ef76264e61582d05eb636e84c52698d28
Aydın UZUN <uzun.aydinn@gmail.com>
2021-10-20 16:39:45 +0300

    Update AnnouncementSessionRepository.java

--- src/main/java/com/works/repositories/_redis/AnnouncementSessionRepository.java
+++ src/main/java/com/works/repositories/_redis/AnnouncementSessionRepository.java
@@ -1,5 +1,7 @@
 package com.works.repositories._redis;
 
+import com.works.entities.Announcement;
+import com.works.entities.Company;
 import com.works.models._redis.AnnouncementSession;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +15,12 @@ public interface AnnouncementSessionRepository extends CrudRepository<Announceme
 
     @Query("select s from sessionannouncement s order by s.id")
     List<AnnouncementSession> findByOrderByIdAsc(Pageable pageable);
+
+    //@Query("select s from sessionannouncement s where s.companyname = ?1")
+    //List<AnnouncementSession> findByAnnouncementSession(String companyname, Pageable pageable);
+
+    //@Query(value = "select s from sessionannouncement s where s.companyname = ?1", nativeQuery = true)
+    //List<AnnouncementSession> findByCompanyNameEquals(String companyname, Pageable pageable);
+
+
 }
