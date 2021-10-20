a8e7ea63991b64ed97c47dd1fce4fa46dff71992
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-20 17:58:33 +0300

    Update LikeSessionRepository

--- src/main/java/com/works/repositories/_redis/LikeSessionRepository.java
+++ src/main/java/com/works/repositories/_redis/LikeSessionRepository.java
@@ -4,10 +4,11 @@ import com.works.models._redis.LikeSession;
 import com.works.models._redis.SurveySession;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-
+@EnableRedisRepositories
 public interface LikeSessionRepository extends CrudRepository<LikeSession,String> {
 
     @Query("select s from sessionlike s order by s.id")
