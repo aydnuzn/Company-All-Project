1ca551f45465be67d52744754914aa224d116121
Mert DUMANLI <77790459+mertdumanlicse@users.noreply.github.com>
2021-10-20 18:24:44 +0300

    Create CustomerElastic.java

---
+++ src/main/java/com/works/models/_elastic/CustomerElastic.java
@@ -0,0 +1,32 @@
+package com.works.models._elastic;
+
+import com.works.utils.Util;
+import lombok.Data;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import javax.persistence.Id;
+
+@Document(indexName = "customerelastic")
+@Data
+public class CustomerElastic {
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String name;
+
+    @Field(type = FieldType.Text)
+    private String surname;
+
+    @Field(type = FieldType.Text)
+    private String email;
+
+    @Field(type = FieldType.Text)
+    private String tel;
+
+    @Field(type = FieldType.Text)
+    private String companyname = Util.theCompany.getCompany_name();
+
+}
