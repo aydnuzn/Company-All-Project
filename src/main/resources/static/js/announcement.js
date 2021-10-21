fe2f7d1f456222beb29d539971d62186d908afa4
Aydın UZUN <uzun.aydinn@gmail.com>
2021-10-21 06:42:31 +0300

    update announcement.js

--- src/main/resources/static/js/announcement.js
+++ src/main/resources/static/js/announcement.js
@@ -1,143 +1,92 @@
 /*jshint esversion: 6 */
 /*jslint latedef:false*/
+/*jshint -W087 */
 
+let url = "http://localhost:8091/rest/admin/announcement/datatable/list";
+let tempUrl = "http://localhost:8091/rest/admin/announcement/datatable/list";
 let searchKey = "";
 $(document).on('keyup', 'input', function () {
     searchKey = this.value;
     if (this.value.trim() == "") {
-        getAnnouncementList(pageableNumber);
+        console.log("redis");
+        tempUrl = url;
+        $('#exampleTable').DataTable().ajax.url(tempUrl).load();
     } else {
-        getAnnouncementListSearch(this.value, pageableNumber);
+        console.log("Elastic -->" + this.value);
+        tempUrl = url + '/' + this.value;
+        console.log("tempUrl = " + tempUrl);
+        $('#exampleTable').DataTable().ajax.url(tempUrl).load();
     }
 });
 
-// Arama var
-//Elastic Arama
-function getAnnouncementListSearch(searchKey, index) {
-    $.ajax({
-        url: 'http://localhost:8091/rest/admin/announcement/list/' + searchKey + "/" + index,
-        type: 'GET',
-        contentType: "application/json",
-        dataType: 'json',
-        success: function (data) {
-            if (data) {
-                console.log(data);
-                pageableNumber = index;
-                oldData = data.RESULT.content;
-                getRows(data.RESULT.content);
-                getPageNumbers(data.COUNTOFPAGE);
-            } else {
-                console.log(data);
+$(document).ready(function() {
+    var tablem = $('#exampleTable').DataTable({
+        //dom: 'Bfrtip',
+        "retrieve": true,
+        "processing": true,
+        "serverSide": true,
+        "searching": true,
+        "paging": true,
+        "dom": '<"top"i>rt<"bottom"lp><"clear">', // for hide default global search box
+        "ajax": ({
+            "url": tempUrl,
+            "type": 'Get',
+            "datatype": 'json',
+            dataSrc: function (d) {
+                debugger;
+                d.draw = d.DRAW;
+                d.recordsTotal = d.COUNT;   //--> Sayfada bulunan toplam kayıt bilgisini veren kısım
+                d.recordsFiltered = d.COUNT;   //--> Paggination için gerekli kısım
+                return d.RESULT;
             }
-        },
-        error: function (err) {
-            console.log(err);
-        }
-    });
-}
-
-//Arama Yok
-let pageableNumber = 1;
-getAnnouncementList(pageableNumber);
-let oldData = [];
-
-function getAnnouncementList(index) {
-    $.ajax({
-        url: 'http://localhost:8091/rest/admin/announcement/list/' + index,
-        type: 'GET',
-        contentType: "application/json",
-        dataType: 'json',
-        success: function (data) {
-            if (data) {
-                console.log(data);
-                pageableNumber = index;
-                oldData = data.RESULT;
-                getRows(data.RESULT);
-                getPageNumbers(data.COUNTOFPAGE);
-            } else {
-                console.log(data);
-            }
-        },
-        error: function (err) {
-            console.log(err);
-        }
-    });
-}
-
-function getRows(data) {
-    let html = ``;
-    for (let i = 0; i < data.length; i++) {
-        const itm = data[i];
-
-        html += `<tr>
-            <td>` + itm.ann_title + `</td>
-            <td>` + itm.ann_brief_description + `</td>
-            <td>` + itm.ann_type + `</td>
-          
-            <td>
-                <div class="ui buttons" style="float: right">
-                    <a href="http://localhost:8091/admin/announcement/` + itm.id + `" class="ui primary button">Düzenle</a>
+        }),
+        "columns": [
+            { "data": "id", "name": "id", "autoWidth": true },
+            { "data": "ann_title", "name": "ann_title", "autoWidth": true },
+            { "data": "ann_brief_description", "name": "ann_brief_description", "autoWidth": true },
+            { "data": "ann_type", "name": "ann_type", "autoWidth": true },
+            {
+                "data": "id", "name": "id", "autoWidth": true,
+                "render": function (data) {
+                    var a = `<div class="ui buttons" style="float: right">
+                    <a href="http://localhost:8091/admin/announcement/`+data+`" class="ui vertical animated primary button">
+                    <div class="hidden content">Düzenle</div>
+                    <div class="visible content">
+                        <i class="edit icon"></i>
+                    </div>
+                    </a>
                     <div class="ya da"></div>
-                    <button onclick="deleteAnnouncement(` + itm.id + `)"   class="ui negative button">Sil</button>
-                </div>                      
-            </td>
-          </tr>`;
-    }
-    $('#announcementTbody').html(html);
-}
+                    <button onclick="deleteAnnouncement(`+data+`)" class="ui vertical animated negative button">
+                    <div class="hidden content">Sil</div>
+                    <div class="visible content">
+                        <i class="trash icon"></i>
+                    </div>
+                    </button>
+                </div> `;
+                    return a;
+                }
+            }
+        ],
+        language: {
+            "zeroRecords": 'Kayıt Bulunamadı.',
+            "infoEmpty": 'Eşleşen Kayıt Bulunamadı.',
+            "infoFiltered": ''
+        }
+    });
+});
 
 function deleteAnnouncement(index) {
-    if (oldData.length === 1) {
-        pageableNumber = pageableNumber - 1;
-    }
     $.ajax({
         url: 'http://localhost:8091/rest/admin/announcement/delete/' + index,
         type: 'DELETE',
         contentType: "application/json",
         dataType: 'json',
         success: function (data) {
-            if (data.STATUS) {
-                getAnnouncementList(pageableNumber);
-            } else {
-                pageableNumber = 1;
-                getAnnouncementList(pageableNumber);
-            }
+            debugger;
+            $('#exampleTable').DataTable().ajax.reload();
         },
         error: function (err) {
             console.log(err);
         }
     });
-}
-
-//Değişirse sadece burası değişecek
-function getPageNumbers(data) {
-    let html = ``;
-    if (data === 1) {
-        html += `
-        <a class="item" onclick="` + getPageableNumber(1) + `">1</a>
-        `;
-    } else if (data === 2 && pageableNumber == 1) {
-        html += `
-        <a class="item" onclick="` + getPageableNumber(1) + `">1</a>
-        <a class="icon item" onclick="` + getPageableNumber(2) + `">
-        <i class="right chevron icon""></i> </a>
-        `;
-    } else if (data === 2 && pageableNumber == 2) {
-        html += `
-        <a class="icon item" onclick="` + getPageableNumber(1) + `">
-        <i class="left chevron icon" ></i>
-        </a>
-        <a class="item" onclick="` + getPageableNumber(2) + `">2</a>
-        `;
-    }
-    $('#announcementPNumber').html(html);
-}
-
-function getPageableNumber(i) {
-    if (searchKey.trim() == "") {
-        return "getAnnouncementList(" + i + ")";
-    } else {
-        let searchKeyTrim = searchKey.trim();
-        return "getAnnouncementListSearch('" + searchKeyTrim + "'" + "," + i + ")";
-    }
 }
