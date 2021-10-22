8c58661fdf6a7c412708a1a23e9bfddf93275224
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-22 16:41:12 +0300

    Update OrderSessionRepository.java

--- src/main/java/com/works/repositories/_redis/OrderSessionRepository.java
+++ src/main/java/com/works/repositories/_redis/OrderSessionRepository.java
@@ -1,16 +1,26 @@
 package com.works.repositories._redis;
-
-import com.works.models._redis.LikeSession;
 import com.works.models._redis.OrderSession;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 @EnableRedisRepositories
+@Transactional
 public interface OrderSessionRepository extends CrudRepository<OrderSession,String> {
 
     @Query("select s from sessionorder s order by s.id")
     List<OrderSession> findByOrderByIdAsc(Pageable pageable);
+
+    /*
+    @Modifying
+    @Query(value = "update sessionorder s set s.order_status = 1 where s.id=?1", nativeQuery = true)
+    Object orderStatusChange(int id);
+     */
+
+
+
 }
