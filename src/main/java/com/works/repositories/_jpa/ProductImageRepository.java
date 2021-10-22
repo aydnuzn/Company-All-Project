f7dce5a4474ea6a7706752d2f38a7acfd7b4861d
Aydın UZUN <uzun.aydinn@gmail.com>
2021-10-22 06:13:59 +0300

    Update ProductImageRepository.java

--- src/main/java/com/works/repositories/_jpa/ProductImageRepository.java
+++ src/main/java/com/works/repositories/_jpa/ProductImageRepository.java
@@ -1,7 +1,11 @@
 package com.works.repositories._jpa;
 
+import com.works.entities.Product;
 import com.works.entities.images.ProductImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductImageRepository extends JpaRepository<ProductImage, Integer> {
+    List<ProductImage> findByProduct_IdEquals(Integer id);
 }
