5c36620916a231274cb50062e0fffa1d91284940
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-22 23:09:07 +0300

    Update LikeRepository.java

--- src/main/java/com/works/repositories/_jpa/LikeRepository.java
+++ src/main/java/com/works/repositories/_jpa/LikeRepository.java
@@ -1,7 +1,17 @@
 package com.works.repositories._jpa;
 
 import com.works.entities.LikeManagement;
+import com.works.entities.projections.LikeInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import javax.transaction.Transactional;
+import java.util.List;
+@Transactional
 public interface LikeRepository extends JpaRepository<LikeManagement, Integer> {
+
+
+    @Query(value = "select sum(score) as totalScore, product_id as productID from like_management group by product_id", nativeQuery = true)
+    List<LikeInfo> findAllLikes();
 }
