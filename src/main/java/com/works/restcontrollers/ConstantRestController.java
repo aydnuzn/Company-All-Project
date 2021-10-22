a99c386d6ee772d14a8c81053bbc8da230580687
Selen KÖSOĞLU <skosoglu3@gmail.com>
2021-10-22 17:13:02 +0300

    Update ConstantRestController.java

--- src/main/java/com/works/restcontrollers/ConstantRestController.java
+++ src/main/java/com/works/restcontrollers/ConstantRestController.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/rest")
 public class ConstantRestController {
     final DistrictRepository districtRepository;
 
