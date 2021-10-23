6a9a63290abc560c812d508f092edc9ffdde5c19
Aydın UZUN <uzun.aydinn@gmail.com>
2021-10-23 10:18:29 +0300

    Update AnnouncementSessionRepository.java

--- src/main/java/com/works/repositories/_redis/AnnouncementSessionRepository.java
+++ src/main/java/com/works/repositories/_redis/AnnouncementSessionRepository.java
@@ -13,18 +13,7 @@ import java.util.List;
 @EnableRedisRepositories
 public interface AnnouncementSessionRepository extends CrudRepository<AnnouncementSession,String> {
 
-    @Query("select s from sessionannouncement s order by s.id")
-    List<AnnouncementSession> findByOrderByIdAsc(Pageable pageable);
-
     List<AnnouncementSession> findByCompanynameEquals(String companyname, Pageable pageable);
-
-
-
-    //@Query("select s from sessionannouncement s where s.companyname = ?1")
-    //List<AnnouncementSession> findByAnnouncementSession(String companyname, Pageable pageable);
-
-    //@Query(value = "select s from sessionannouncement s where s.companyname = ?1", nativeQuery = true)
-    //List<AnnouncementSession> findByCompanyNameEquals(String companyname, Pageable pageable);
-
+    List<AnnouncementSession> findByCompanynameEquals(String companyname);
 
 }
