42956194f6bdb09663342083fc501219ffe12020
Aydın UZUN <uzun.aydinn@gmail.com>
2021-10-23 16:27:17 +0300

    Update AdminController.java

--- src/main/java/com/works/controllers/adminpanel/AdminController.java
+++ src/main/java/com/works/controllers/adminpanel/AdminController.java
@@ -1,10 +1,11 @@
 package com.works.controllers.adminpanel;
 
-import com.works.entities.security.User;
+import com.works.entities.projections.DashboardInfo;
+import com.works.repositories._jpa.AnnouncementRepository;
 import com.works.repositories._jpa.UserRepository;
 import com.works.utils.Util;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,26 +17,23 @@ public class AdminController {
 
     final String rvalue = "adminpanel/dashboard/";
     final UserRepository userRepository;
+    final AnnouncementRepository announcementRepository;
 
-    public AdminController(UserRepository userRepository) {
+    public AdminController(UserRepository userRepository, AnnouncementRepository announcementRepository) {
         this.userRepository = userRepository;
+        this.announcementRepository = announcementRepository;
     }
 
+
     @GetMapping("")
-    public String dashboard() {
-        return rvalue + "dashboard";
+    public String dashboard(Model model) {
+        Optional<DashboardInfo> optionalDashboardInfo = announcementRepository.getViewDashboard(Util.theCompany.getId());
+        model.addAttribute("dashboardInfo", optionalDashboardInfo.get());
+        return "adminpanel/dashboard/dashboard";
     }
 
     @GetMapping("/dashboard")
-    public String dashboard2() {
-        getCompany();
-        return rvalue + "dashboard";
-    }
-
-    public void getCompany() {
-        Optional<User> optUser = userRepository.findByEmailEquals(SecurityContextHolder.getContext().getAuthentication().getName());
-        if (optUser.isPresent()) {
-            Util.theCompany = optUser.get().getCompany();
-        }
+    public String dashboard2(Model model) {
+        return "redirect:/admin";
     }
 }
