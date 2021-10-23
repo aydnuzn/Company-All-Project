df504a7f2a4625730988a498c9424d15cb4866f8
Aydın UZUN <uzun.aydinn@gmail.com>
2021-10-23 16:27:24 +0300

    Create DashboardInfo.java

---
+++ src/main/java/com/works/entities/projections/DashboardInfo.java
@@ -0,0 +1,17 @@
+package com.works.entities.projections;
+
+public interface DashboardInfo {
+    String getTotalAdvertisement();
+    String getTotalContent();
+    String getTotalCustomer();
+    String getTotalActiveCustomer();
+    String getTotalPassiveCustomer();
+    String getTotalAnnouncement();
+    String getTotalActiveAnn();
+    String getTotalPassiveAnn();
+    String getTotalProduct();
+    String getTotalLike();
+    String getTotalSurvey();
+    String getTotalOrder();
+
+}
