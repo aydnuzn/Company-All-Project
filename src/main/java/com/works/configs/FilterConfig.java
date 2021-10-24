6453a8d35720f44d8d05e45eaca639aa0dae79d4
Mert DUMANLI <77790459+mertdumanlicse@users.noreply.github.com>
2021-10-24 18:07:18 +0300

    Update FilterConfig.java

--- src/main/java/com/works/configs/FilterConfig.java
+++ src/main/java/com/works/configs/FilterConfig.java
@@ -1,12 +1,13 @@
 package com.works.configs;
 
+import com.works.entities.Company;
 import com.works.entities.security.User;
 import com.works.repositories._jpa.UserRepository;
+import com.works.services.UserService;
 import com.works.utils.Util;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -17,12 +18,13 @@ import java.util.Optional;
 @Configuration
 public class FilterConfig implements Filter {
 
-    //final HttpServletRequest httpServletRequest;
-    //final HttpServletResponse httpServletResponse;
     final UserRepository userRepository;
+    final UserService userService;
 
-    public FilterConfig(UserRepository userRepository) {
+    public FilterConfig(UserRepository userRepository, UserService userService) {
         this.userRepository = userRepository;
+        this.userService = userService;
+        System.out.println("The System Started!");
     }
 
     @Override
@@ -32,6 +34,7 @@ public class FilterConfig implements Filter {
 
         req.setCharacterEncoding("utf-8");
         res.setCharacterEncoding("utf-8");
+        userService.info(req);
         getCompany();
         filterChain.doFilter(req, res);
     }
@@ -40,26 +43,20 @@ public class FilterConfig implements Filter {
         try {
             Optional<User> optUser = userRepository.findByEmailEquals(SecurityContextHolder.getContext().getAuthentication().getName());
             if (optUser.isPresent()) {
-                //System.out.println("The Company : " + optUser.get().getCompany());
                 Util.theCompany = optUser.get().getCompany();
+            } else {
+                Util.theCompany = new Company();
             }
         } catch (Exception e) {
-            //swagger tarafında security nesnesi gözardı edildiği için patlıyor.
+            Util.theCompany = new Company();
             System.out.println("Swagger Run");
         }
 
     }
 
-    // Proje ilk çalıştıgında 1 kez tetiklenir
-    @PostConstruct
-    private void postConstruct() {
-
-    }
-
-    // Proje kapanırken tetikleniyor
     @PreDestroy
     public void destroy() {
-        System.out.println("Callback triggered - @PreDestroy.");
+        System.out.println("The System Shut Down!");
     }
 
 
