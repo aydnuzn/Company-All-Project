352539de98f1513c1bf93a965ec7f8ca8c262558
Aydın UZUN <uzun.aydinn@gmail.com>
2021-10-24 17:39:11 +0300

    fixed SqlQuery for GalleryRepository

--- src/main/java/com/works/repositories/_jpa/GalleryRepository.java
+++ src/main/java/com/works/repositories/_jpa/GalleryRepository.java
@@ -6,12 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GalleryRepository extends JpaRepository<Gallery, Integer> {
     List<Gallery> findByCompany_IdEquals(Integer id);
 
     @Query(value = "SELECT g.id as gallery_id, g.gallery_title, gi.gallery_image_url FROM `gallery_image` as gi\n" +
             "RIGHT JOIN gallery as g ON g.id = gi.gallery_id\n" +
-            "Where g.company_id = ?1 GROUP BY gi.gallery_id;",nativeQuery = true)
+            "Where g.company_id = ?1 GROUP BY g.id",nativeQuery = true)
     List<GalleryInfo> getGalleryInfo(Integer company);
+
+    Optional<Gallery> findByGalleryImages_IdEquals(Integer id);
+
+
 }
