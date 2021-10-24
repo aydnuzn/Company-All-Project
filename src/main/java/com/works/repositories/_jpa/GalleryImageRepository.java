235aa618dfb88cfdbb2e2d9dbd9d6d1b1b9a5ea3
Aydın UZUN <uzun.aydinn@gmail.com>
2021-10-24 17:40:08 +0300

    added control query for GalleryImage

--- src/main/java/com/works/repositories/_jpa/GalleryImageRepository.java
+++ src/main/java/com/works/repositories/_jpa/GalleryImageRepository.java
@@ -3,5 +3,9 @@ package com.works.repositories._jpa;
 import com.works.entities.images.GalleryImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GalleryImageRepository extends JpaRepository<GalleryImage, Integer> {
+    List<GalleryImage> findByGallery_IdEquals(Integer id);
+
 }
