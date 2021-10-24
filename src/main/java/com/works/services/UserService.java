4c340a9f79489131e0d648c96c14844c4a50ce65
Mert DUMANLI <77790459+mertdumanlicse@users.noreply.github.com>
2021-10-24 18:13:22 +0300

    Add Logger

--- src/main/java/com/works/services/UserService.java
+++ src/main/java/com/works/services/UserService.java
@@ -1,8 +1,9 @@
 package com.works.services;
 
-import com.works.entities.Company;
+import com.works.entities.Logger;
 import com.works.entities.security.Role;
 import com.works.entities.security.User;
+import com.works.repositories._jpa.LoggerRepository;
 import com.works.repositories._jpa.UserRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -16,6 +17,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -23,6 +26,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,9 +34,13 @@ import java.util.Optional;
 @Transactional
 public class UserService extends SimpleUrlLogoutSuccessHandler implements UserDetailsService, LogoutSuccessHandler {
     final UserRepository uRepo;
+    final UserRepository userRepository;
+    final LoggerRepository loggerRepository;
 
-    public UserService(UserRepository uRepo) {
+    public UserService(UserRepository uRepo, UserRepository userRepository, LoggerRepository loggerRepository) {
         this.uRepo = uRepo;
+        this.userRepository = userRepository;
+        this.loggerRepository = loggerRepository;
     }
 
     // security login
@@ -72,6 +80,49 @@ public class UserService extends SimpleUrlLogoutSuccessHandler implements UserDe
     public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
         System.out.println("onLogoutSuccess Call ");
         httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/homepanel/login/login");
+    }
+
+    public void info(HttpServletRequest req) throws IOException {
+        Logger logger = new Logger();
+        String email = "";
+        try {
+            Authentication aut = SecurityContextHolder.getContext().getAuthentication();
+            email = aut.getName(); // username
+        } catch (Exception e) {
+
+        }
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+                .getRequestAttributes()).getRequest();
+        String ip = request.getRemoteAddr();
+        //System.out.println("ip " + ip);
+
+        String session = req.getSession().getId();
+        //System.out.println("session :" + session);
+
+        Optional<User> user = userRepository.findByEmailEquals(email);
+        if (user.isPresent()) {
+            logger.setLname(user.get().getName());
+            logger.setLsurname(user.get().getSurname());
+            String roles = "";
+            for (Role item : user.get().getRoles()) {
+                roles += item.getRo_name() + ", ";
+            }
+            if (roles.length() > 0) {
+                roles = roles.substring(0, roles.length() - 2);
+            }
+            logger.setLroles(roles);
+        }
+
+        logger.setLemail(email);
+        logger.setLsessionId(session);
+        logger.setLIp(ip);
+
+        logger.setLUrl(req.getRequestURI());
+        logger.setLDate(new Date());
+
+        loggerRepository.save(logger);
+
     }
 
 }
